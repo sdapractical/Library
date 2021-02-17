4a01e3a218630637ec45ea64e77fd93e49770523
Sabina Antosevska <antoshevska@gmail.com>
2021-02-17 19:16:27 +0200

    Created an sql script for database creation

--- src/main/java/sql/librarySQL.sql
+++ src/main/java/sql/librarySQL.sql
@@ -6,7 +6,7 @@ USE `library`;
 CREATE DATABASE IF NOT EXISTS library;
 
 
-CREATE TABLE IF NOT EXISTS `readers` (
+CREATE TABLE IF NOT EXISTS `reader` (
     `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
     `firstName` VARCHAR(100) NOT NULL DEFAULT 'Unknown',
     `lastName` VARCHAR(100) NOT NULL DEFAULT 'Unknown',
@@ -15,17 +15,16 @@ CREATE TABLE IF NOT EXISTS `readers` (
     PRIMARY KEY (`id`))
     AUTO_INCREMENT = 1;
 
-CREATE TABLE IF NOT EXISTS `books` (
+CREATE TABLE IF NOT EXISTS `book` (
     `id`      BIGINT(20)   NOT NULL AUTO_INCREMENT,
     `title`   VARCHAR(255) NOT NULL DEFAULT 'Unknown',
     `authors` VARCHAR(200) NULL     DEFAULT NULL,
     `genre`   VARCHAR(50)  NULL     DEFAULT NULL,
     `publishingYear` DATE  NULL     DEFAULT NULL,
     `borrowed` BOOLEAN     DEFAULT FALSE,
-    `reader`   BOOLEAN     DEFAULT FALSE,
-    `readers_id` BIGINT(20) NULL,
+    `reader_id` BIGINT(20) NULL,
     `dueDate` DATE,
     PRIMARY KEY (`id`),
-    CONSTRAINT `fk_readers`
-    FOREIGN KEY (`readers_id`) REFERENCES readers (`id`) ON DELETE SET NULL)
+    CONSTRAINT `fk_reader`
+    FOREIGN KEY (`reader_id`) REFERENCES reader (`id`) ON DELETE SET NULL)
     AUTO_INCREMENT = 1;
